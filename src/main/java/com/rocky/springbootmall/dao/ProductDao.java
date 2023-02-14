cb37267e6992f8f39508760d85e764c38f88b0a3
rocky <mindynelson25@hotmail.com>
2023-02-14 21:37:20 +0800

    商品功能 實作查詢商品列表(基礎實作)

--- src/main/java/com/rocky/springbootmall/dao/ProductDao.java
+++ src/main/java/com/rocky/springbootmall/dao/ProductDao.java
@@ -3,7 +3,11 @@ package com.rocky.springbootmall.dao;
 import com.rocky.springbootmall.dto.ProductRequest;
 import com.rocky.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
--- src/main/java/com/rocky/springbootmall/service/ProductService.java
+++ src/main/java/com/rocky/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.rocky.springbootmall.service;
 import com.rocky.springbootmall.dto.ProductRequest;
 import com.rocky.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
 
     Product getProductById(Integer productId);
 
@@ -12,4 +16,6 @@ public interface ProductService {
     void updateProduct(Integer productId,ProductRequest productRequest);
 
     void deleteProductById(Integer productId);
+
+
 }
